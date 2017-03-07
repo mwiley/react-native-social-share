151764200af3aa6b1d5692ea7d8033d979fca2b4
Mason Wiley <masonwiley92@gmail.com>
2017-03-07 14:55:47 -0800

    fix iOS project target

---
+++ ReactNativeSocialShare/ReactNativeSocialShare.h
@@ -0,0 +1,13 @@
+//
+//  ReactNativeSocialShare.h
+//  ReactNativeSocialShare
+//
+//  Created by Mason Wiley on 3/7/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface ReactNativeSocialShare : NSObject
+
+@end
---
+++ ReactNativeSocialShare/ReactNativeSocialShare.m
@@ -0,0 +1,13 @@
+//
+//  ReactNativeSocialShare.m
+//  ReactNativeSocialShare
+//
+//  Created by Mason Wiley on 3/7/17.
+//  Copyright © 2017 Facebook. All rights reserved.
+//
+
+#import "ReactNativeSocialShare.h"
+
+@implementation ReactNativeSocialShare
+
+@end
